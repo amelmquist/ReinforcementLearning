113f6106eca8238920d1d9ec53db814ac6d73d99
Asher Elmquist <amelmquist@wisc.edu>
2018-07-31 13:10:37 -0500

    improve duel-double-dqn

--- agents/dddqn.py
+++ agents/dddqn.py
@@ -12,23 +12,25 @@ import random
 import sys
 import numpy as np
 from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from keras.models import Sequential, Model, load_model
+from keras.layers import Input, Dense, Dropout, Activation, BatchNormalization, Conv2D, MaxPool2D, Flatten, Lambda, Concatenate
 from keras.optimizers import SGD, Adam
+from keras import backend as K
 
 
 #hyper parameters
-num_filters = 8
-filter_size = (8,8)
+num_filters = 16
+filter_size = 4
 learning_rate = 0.001
-num_dense = 64
-mem_size = 200000
+num_dense = 256
+mem_size = 2000
 max_epsilon = 1.0
 min_epsilon = 0.01
 annealing_steps = 1000
-num_epochs = 1
-batch_size = 50
-discout_rate = .99
+batch_size = 100
+num_batches = 1
+discout_rate = .95
+target_step = 0.001
 
 class DDDQNAgent:
     #constructor
@@ -45,25 +47,60 @@ class DDDQNAgent:
 
         self.main_model = self.network()
         self.target_model = self.network()
+        # self.update_target()
 
     #network
     def network(self):
-        model = Sequential();
-        model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu', input_shape=self.state_shape))
-        model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu'))
+        #sequential model
+        # model = Sequential();
+        # model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu', input_shape=self.state_shape))
         # model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu'))
         # model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu'))
+        # model.add(Conv2D(num_filters, filter_size, padding='same', activation='relu'))
+        #
+        # #this is where it should split
+        #
+        # #continue on with no split
+        # model.add(Flatten())
+        # model.add(Dense(num_dense, activation='relu'))
+        # model.add(Dense(num_dense, activation='relu'))
+        # model.add(Dense(self.action_size, activation='linear'))
+        # model.compile(loss='mse', optimizer=SGD(lr=learning_rate))
+        # model.compile(loss=self.huber_loss, optimizer=Adam(lr=learning_rate))
 
-        #this is where it should split
+        #dueling model
+        input = Input(shape=self.state_shape)
+        conv0 = Conv2D(num_filters, 2*filter_size, strides=(4,4), padding='valid', activation='relu')(input)
+        conv1 = Conv2D(2*num_filters, filter_size, strides=(2,2), padding='valid', activation='relu')(conv0)
+        conv2 = Conv2D(2*num_filters, filter_size, strides=(1,1), padding='valid', activation='relu')(conv1)
+        flatten = Flatten()(conv2)
+        dense0 = Dense(num_dense, activation='relu')(flatten)
+        dense1 = Dense(num_dense, activation='relu')(dense0)
 
-        #continue on with no split
-        model.add(Flatten())
-        model.add(Dense(num_dense, activation='relu'))
-        model.add(Dense(num_dense, activation='relu'))
-        model.add(Dense(self.action_size, activation='linear'))
-        model.compile(loss='mse', optimizer=SGD(lr=learning_rate))
+        out = Dense(self.action_size + 1,activation='linear')(dense1) # add 1 more unit for value
+
+        # out = value + avantage - mean(advantage)
+        output = Lambda(lambda a: K.expand_dims(a[:, 0], axis=-1) + a[:, 1:] - K.mean(a[:, 1:], keepdims=True, axis=1))(out)
+
+        model = Model(input, output)
+
+        model.summary()
+
+        model.compile(loss="mse", optimizer=Adam(lr=learning_rate))
 
         return model
+
+    def huber_loss(self, target, prediction):
+        # sqrt(1+error^2)-1
+        error = prediction - target
+        return K.mean(K.sqrt(1+K.square(error))-1, axis=-1)
+
+    def update_target(self):
+        main_weights = self.main_model.get_weights()
+        target_weights = self.target_model.get_weights()
+        for i in range(len(main_weights)):
+            target_weights[i] = target_step*main_weights[i] + (1-target_step)*target_weights[i]
+        self.target_model.set_weights(target_weights)
 
     #load models
     def load(self, main_name, target_name=""):
@@ -88,42 +125,111 @@ class DDDQNAgent:
 
     #replay based on samples from memory
     def replay(self):
-        if(len(self.memory) < batch_size):
-            return
-        #sample the memory
-        minibatch = random.sample(self.memory, batch_size)
+        loss = 'NAN'
+        if(len(self.memory) >= batch_size):
+            loss = 0
+            #sample the memory
+            for i in range(num_batches):
+                minibatch = random.sample(self.memory, batch_size)
+                # minibatch_list = minibatch
+                # minibatch = np.asarray(minibatch)
 
-        for sample in minibatch:
-            state, action, reward, next_state, done = sample
-            #assume the target is exactly what we think for now
-            target = self.main_model.predict(state)
+                # print("minibatch shape",minibatch.shape)
+                # print("minibatch type",type(minibatch))
+                # print("shape of states:",minibatch[:,0].shape)
+                states = []
+                actions = np.array([]).astype(int)
+                rewards = np.array([])
+                next_states = []
+                dones = np.array([]).astype(int)
+                for m in minibatch:
+                    states.append(m[0])
+                    actions = np.append(actions,m[1])
+                    rewards = np.append(rewards,m[2])
+                    next_states.append(m[3])
+                    dones = np.append(dones,m[4])
 
-            #if we are done, we know exactly the reward so modify reward output
-            if(done):
-                target[0][action] = reward
+                states = np.asarray(states)
+                next_states = np.asarray(next_states)
 
-            #if sample is not from the end, use a predicted, discounted future reward
-            else:
-                future_reward = np.amax(self.target_model.predict(next_state)[0])
-                target[0][action] = reward + self.gamma*future_reward
+                # print("States shape:",np.asarray(states).shape)
+                # print("actions shape:",actions.shape)
+                # print("rewards shape:",rewards.shape)
+                # print("next_states shape:",np.asarray(next_states).shape)
+                # print("dones shape:",dones.shape)
 
-            #fit the main model to the new reward
-            self.main_model.fit(state, target, epochs=num_epochs, verbose=0)
+                # exit(1)
+                # #do batch training
+                # states = np.reshape(minibatch[:,0],[len(minibatch_list), minibatch[0,0].shape[0],minibatch[0,0].shape[1],minibatch[0,0].shape[2]])
+                # print("State shape:",states.shape)
 
-            #bring the target_model a bit closer to the main model
-            self.target_model.fit(state, target, epochs=num_epochs, verbose=0)
+                # exit(1)
+                #
+                # actions = minibatch[:,1]
+                # rewards = minibatch[:,2]
+                # next_states = minibatch[:,3]
+                # dones = minibatch[:,4]
+
+                # print("next states shape:",next_states.shape)
+                # print("next states:",next_states)
+
+                actions_newstate = np.argmax(self.main_model.predict(next_states),axis=1)
+                target_qvalues_newstate = self.target_model.predict(next_states)
+                double_q = target_qvalues_newstate[range(target_qvalues_newstate.shape[0]), actions_newstate]
+
+                done_multiplier = 1-dones   #0 if done, 1 if not
+                target_q = rewards + self.gamma*double_q+done_multiplier
+
+
+                q_values = self.main_model.predict(states)
+                for i in range(q_values.shape[0]):
+                    q_values[i, int(actions[i])] = target_q[i]
+
+                loss += self.main_model.train_on_batch(states,q_values)
+
+
+
+            # for sample in minibatch:
+            #     state, action, reward, next_state, done = sample
+            #     #assume the target is exactly what we think for now
+            #     target = self.main_model.predict(state)
+            #
+            #     #if we are done, we know exactly the reward so modify reward output
+            #     if(done):
+            #         target[0][action] = reward
+            #
+            #     #if sample is not from the end, use a predicted, discounted future reward
+            #     else:
+            #         future_reward = np.amax(self.target_model.predict(next_state)[0])
+            #         target[0][action] = reward + self.gamma*future_reward
+            #
+            #     #fit the main model to the new reward
+            #     self.main_model.fit(state, target, epochs=num_epochs, verbose=0)
+            #
+            # #bring the target_model a bit closer to the main model
+            # self.update_target()
+            # # self.target_model.fit(state, target, epochs=num_epochs, verbose=0)
 
         #update epsilon
         self.epsilon -= self.epsilon_decay
         if(self.epsilon < self.min_epsilon): self.epsilon=self.min_epsilon
 
+        self.update_target()
+
+        return loss
+
     def remember(self, state, action, reward, next_state, done):
-        state = np.reshape(state, [1,self.state_shape[0],self.state_shape[1],self.state_shape[2]])
-        next_state = np.reshape(next_state, [1,self.state_shape[0],self.state_shape[1],self.state_shape[2]])
-        experience = (state, action, reward, next_state, done)
-        if len(self.memory) + 1 >= mem_size:
-            self.memory.pop()
-        self.memory.appendleft(experience)
+        # state = np.reshape(np.asarray(state), [1,self.state_shape[0],self.state_shape[1],self.state_shape[2]])
+        # next_state = np.reshape(np.asarray(next_state), [1,self.state_shape[0],self.state_shape[1],self.state_shape[2]])
+        # experience = (state, action, reward, next_state, done)
+
+        # if len(self.memory) + 1 >= mem_size:
+        #     self.memory.pop()
+        if(done):
+            done = 1
+        else:
+            done = 0
+        self.memory.append((state, action, reward, next_state, done))
 
 
 
--- example_grid.py
+++ example_grid.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import time
 
 game_size = 5   #size of the square game grid
-num_episodes = 1000   #run through this many episodes
-max_num_turns = 20  #only allow the agent to take up to this many turns
+num_episodes = 1000  #run through this many episodes
+max_num_turns = 30  #only allow the agent to take up to this many turns
 render = False
 
 if __name__ == "__main__":
@@ -20,7 +20,92 @@ if __name__ == "__main__":
     agent = DDDQNAgent(state.shape, env.actions)
 
     reward_list = []
+    time_total_list = []
+    time_train_list = []
+    time_step_list = []
+    time_act_list = []
     for ep in range(num_episodes):
+        state = env.reset()
+        done = False
+        turns = 0
+        accumulated_reward = 0  #reward over each step
+
+        time_total = 0
+        time_train = 0
+        time_step = 0
+        time_act = 0
+        start = time.time()
+        while(turns < max_num_turns and not done):
+            pt0 = time.time()
+            #have the agent select an action
+            action = agent.act(state)
+            pt1 = time.time()
+            time_act += pt1-pt0
+            #find the result of that action
+            next_state, reward, done = env.step(action)
+            pt2 = time.time()
+            time_step = pt2-pt1
+
+            #
+
+
+
+            #remember that action
+            agent.remember(state, action, reward, next_state, done)
+
+            #update totals
+            accumulated_reward += reward
+            state = next_state
+            turns += 1
+
+            if(render):
+                env.render()
+
+
+        #after each episode, have the agent replay from its memory
+        loss = agent.replay()
+
+        #print out the episode stats
+        if(ep%10==0):
+            print("Episode:",ep,"| epsilon:","{0:.3f}".format(agent.epsilon), \
+            "| total_reward:",accumulated_reward," | loss:","{0:.3f}".format(loss))
+        reward_list.append((ep, accumulated_reward))
+
+        pt3 = time.time()
+
+        end = time.time()
+        time_train = end-pt3
+        time_total = end-start
+
+        #update the times
+        time_total_list.append((ep,time_total))
+        time_train_list.append((ep,time_train))
+        time_step_list.append((ep,time_step))
+        time_act_list.append((ep,time_act))
+
+    agent.save("save/dqn_weights.h5")
+    # print(reward_list)
+    #plot the results
+    plt.figure(0)
+    plt.plot(*zip(*reward_list))
+    plt.xlabel('episode')
+    plt.ylabel('total reward')
+    plt.show()
+
+    plt.figure(1)
+    plt.plot(*zip(*time_total_list))
+    plt.plot(*zip(*time_train_list))
+    plt.plot(*zip(*time_step_list))
+    plt.plot(*zip(*time_act_list))
+    plt.legend(['total time','training time','step time',"act time"])
+    plt.xlabel('episode')
+    plt.ylabel('time')
+    plt.show()
+
+    # play the results
+    agent.epsilon = 0
+
+    for ep in range(10):
         state = env.reset()
         done = False
         turns = 0
@@ -35,55 +120,11 @@ if __name__ == "__main__":
             #find the result of that action
             next_state, reward, done = env.step(action)
 
-            #remember that action
-            agent.remember(state, action, reward, next_state, done)
+            print("Action:",action)
 
             #update totals
             accumulated_reward += reward
             state = next_state
             turns += 1
 
-            if(render):
-                env.render()
-
-
-
-        #print out the episode stats
-        print("Episode:",ep,"| epsilon:","{0:.3f}".format(agent.epsilon), "| total_reward:",accumulated_reward)
-        reward_list.append((ep, accumulated_reward))
-
-        #after each episode, have the agent replay from its memory
-        agent.replay()
-
-    agent.save("save/dqn_weights.h5")
-    # print(reward_list)
-    #plot the results
-    plt.plot(*zip(*reward_list))
-    plt.show()
-
-    #play the results
-    # agent.epsilon = 0
-    #
-    # for ep in range(10):
-    #     state = env.reset()
-    #     done = False
-    #     turns = 0
-    #     accumulated_reward = 0  #reward over each step
-    #
-    #
-    #     while(turns < max_num_turns and not done):
-    #
-    #         #have the agent select an action
-    #         action = agent.act(state)
-    #
-    #         #find the result of that action
-    #         next_state, reward, done = env.step(action)
-    #
-    #         print("Action:",action)
-    #
-    #         #update totals
-    #         accumulated_reward += reward
-    #         state = next_state
-    #         turns += 1
-    #
-    #         env.render()
+            env.render()
